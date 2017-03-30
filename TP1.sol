dc776852ebc1f69c2c3cd9d14b1c3ad415576886
MarionDalciet <mdalciet@hotmail.fr>
2017-03-30 21:17:54 +0200

    Add files via upload

---
+++ TP1.sol
@@ -0,0 +1,106 @@
+﻿pragma solidity ^0.4.0;
+
+// Nom du contrat
+contract SafeVote {
+
+    address respo; // responsable du bureau de vote, celui qui execute le contrat. address permet de désigner une adresse Ethereum (20bytes).
+
+    
+    struct Voter {    // on crée l'"objet" électeur
+
+        bool voted; // on sait s'il a deja voté ( true) ou pas ( false)
+        uint bulletin; // l'id du choix pour lequel il a décidé de voter
+        address delegate;  // son adresse ethereum
+    }
+
+    
+    
+    // on créer un objet choix qui contient l'id du choix plus le nombre de voix qu'on va incrémenter au fur et a mesure
+    
+    struct Choix {     // On crée l'objet choix
+     uint idbulletin;
+     uint NbVote;
+
+    }
+    
+    Choix[] liste_choix ; // on crée une liste des propostions possibles et du nombre de voix correspondant
+    
+    function ajoutChoix( uint choix)    // créer une focntion qui remplit cette liste au fur et a mesure qu'on ajoute des voteurs
+
+    {
+        uint i=0;
+        if(msg.sender==respo)
+        {
+            liste_choix.push(Choix({idbulletin:choix,NbVote:0}));
+        }
+    }
+    
+    
+    Voter[] voters;
+    
+        function ajoutVotant( uint choix, address adresse)    // créer une focntion qui remplit cette liste au fur et a mesure qu'on ajoute des voteurs
+
+    {
+        uint i=0;
+        if(msg.sender==respo)
+        {
+            voters.push(Voter({voted:false,bulletin:choix,delegate:adresse}));
+        }
+    }
+    
+   
+        function SafeVote()
+    {
+        respo=msg.sender;
+        liste_choix= liste_choix;
+        voters=voters;
+        
+    }
+        
+
+    
+    function vote() // on parcours la liste des votants et pour chaque votant on implemente l'objet de la liste de choix correspondant a son choix
+    {
+        
+        for (uint i = 0; i < voters.length; i++) // pour chaque électeur de la liste 
+        
+        {
+            
+            for (uint j= 0; j < liste_choix.length; j++) // on trouve le bulletin correspondant à son choix 
+            {
+                if(voters[i].bulletin == liste_choix[j].idbulletin)
+                {
+                    liste_choix[j].NbVote += 1; // et on rajoute une voix pour ce choix
+                }
+            }
+            
+        voters[i].voted = true; // l'électeur a voté. 
+            
+        }
+        
+    }
+    
+    Choix gagnant;
+    
+        function Resultat()
+    {
+        uint i=0;
+        uint maxdevote=0;
+       
+        while(i<=liste_choix.length)
+        {
+            if(liste_choix[i].NbVote>=maxdevote)
+            {
+                maxdevote=liste_choix[i].NbVote;
+            }
+          i=i+1;  
+        }
+        if(liste_choix[i].idbulletin==i)
+        {
+            gagnant=liste_choix[i];
+        }
+    }
+    
+    event finduvote(uint Tempsduvote);
+    
+}
---
+++ TP2.sol
@@ -0,0 +1,59 @@
+﻿ pragma solidity ^0.4.0;
+contract Fidelitas {
+    
+    struct Carte {  // On crée un objet carte de fidélité
+        uint8 idCarte;
+        uint8 points;
+    }
+    
+    Carte[] liste; // liste des cartes de fidelités
+    
+    uint8 numeroCarte = 0; //affectation d'un numéro de carte, qui sera incrémenté de 1 à chaque nouvelle carte   
+    
+    function creationCarte(){
+        uint8 init = 0;
+        if(caissier==msg.sender) //seulement s'il s'agit du caisier qui effectue la création
+        {
+            liste.push(Carte({idCarte:numeroCarte,points:init}));
+            numeroCarte++;
+        }
+    }
+    
+ 
+    struct Client {                   // on crée une classe client
+        
+        uint8 numCarte;
+        address delegate;
+    }
+    
+    address caissier;   //Le caissier est celui qui execute le contrat
+
+    Client[] registre; // liste des clients
+
+    function Fidelitas (uint8 _numProposals) {  // definit la longueur de la liste de propositions constructeur
+        caissier = msg.sender; // prend l'adresse de celui qui excecute
+        liste = liste;
+        registre = registre;
+        
+    }
+   
+    function ajoutClient(uint8 numeroDeLaCarte, address adresseDuClient){
+           if(caissier==msg.sender)
+        {
+            registre.push(Client({numCarte:numeroDeLaCarte,delegate:adresseDuClient}));
+            numeroCarte++;
+        }
+    }
+    
+    function ajoutPoints(uint8 num, uint8 nbPoints){
+        uint i = 0;
+        while(i<=liste.length)
+        {
+            if(liste[i].idCarte==num)
+            {
+                liste[i].points = liste[i].points + nbPoints;
+            }
+            i++;
+        }
+    }
+}
